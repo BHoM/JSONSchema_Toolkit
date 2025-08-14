75681ee4f218dc518e42818135c64931c4215731
Isak Näslund <isak.naslund@burohappold.com>
2025-08-14 13:21:13 +0100

    Add create methods to support JsonSChema generation

---
+++ JsonSchema_Engine/Create/JsonSchema.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BH.oM.JsonSchema;
+
+namespace BH.Engine.JsonSchema
+{
+    public static partial class Create 
+    {
+        /*******************************************/
+        /**** Public Methods                    ****/
+        /*******************************************/
+
+        public static Schema SchemaSingelKeyword(ISchemaKeyWord keyWord)
+        {
+            return new Schema { Keywords = new List<ISchemaKeyWord> { keyWord } };
+        }
+
+        /*******************************************/
+
+        public static Schema Schema(SchemaType schemaType, bool addNullIfNullable = false)
+        {
+            return SchemaSingelKeyword(Create.TypeKeyword(schemaType));
+        }
+
+        /*******************************************/
+
+        public static Schema Schema(Type type)
+        {
+            return SchemaSingelKeyword(Create.TypeKeyword(type));
+        }
+
+        /*******************************************/
+    }
+}
---
+++ JsonSchema_Engine/Create/TypeKeyword.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BH.oM.JsonSchema;
+
+namespace BH.Engine.JsonSchema
+{
+    public static partial class Create 
+    {
+        /*******************************************/
+        /**** Public Methods                    ****/
+        /*******************************************/
+
+        public static TypeKeyword TypeKeyword(SchemaType schemaType, bool addNullIfNullable = true)
+        {
+            bool addNull = false;
+            if (addNullIfNullable)
+            {
+                switch (schemaType)
+                {
+                    //not nullable types or null -> do not add null
+                    case SchemaType.boolean:
+                    case SchemaType.integer:
+                    case SchemaType.number:
+                    case SchemaType.@null:
+                        addNull = false;
+                        break;
+                    //nullable types -> add null
+                    case SchemaType.array:
+                    case SchemaType.@object:
+                    case SchemaType.@string:
+                    default:
+                        addNull = true;
+                        break;
+                }
+            }
+
+            SchemaType[] types;
+            if (addNull)
+                types = new SchemaType[] { schemaType, SchemaType.@null };
+            else
+                types = new SchemaType[] { schemaType };
+
+            return new TypeKeyword { Type = types };
+        }
+
+        /*******************************************/
+
+        public static TypeKeyword TypeKeyword(Type type)
+        {
+            bool addNull = type.IsNullable();
+
+            SchemaType schemaType = type.SchemaType();
+
+            SchemaType[] types;
+            if (addNull)
+                types = new SchemaType[] { schemaType, SchemaType.@null };
+            else
+                types = new SchemaType[] { schemaType };
+
+            return new TypeKeyword { Type = types };
+        }
+
+        /*******************************************/
+
+        private static bool IsNullable(this Type type)
+        {
+            if (!type.IsValueType)
+                return true; // ref-type
+            if (Nullable.GetUnderlyingType(type) != null)
+                return true; // Nullable<T>
+            return false; // value-type
+        }
+
+        /*******************************************/
+    }
+}
