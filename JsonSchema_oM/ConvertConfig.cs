803287e9fb60c48140a99b51172b5aa2dacc6af8
Isak Näslund <isak.naslund@burohappold.com>
2025-08-18 11:39:00 +0100

    Expand docs of ConvertConfig

--- JsonSchema_oM/ConvertConfig.cs
+++ JsonSchema_oM/ConvertConfig.cs
@@ -15,7 +15,9 @@ namespace BH.oM.JsonSchema
         [Description("Bool indicating if id should be included in top level schema or not.")]
         public virtual bool IncludeId { get; set; } = true;
 
-        [Description("Bool indicating if types used by the parent type should be linked to as ref, or fully expanded.")]
+        [Description("Bool indicating if types used by the parent type should be linked to as ref, or fully expanded.\n" +
+                     "Generally strongly recomended to set this boolean to true for the general case, as setting this to true will expand content of all interr types, as well as their subtype, into the main schema file.\n" +
+                     "For types with abstract or interface properties, this will get even more extreme as all option will be populated. Generally only consider setting this to true for types with few levels of nested proeprty types, and generally no interface types on properties.")]
         public virtual bool TypesAsRef { get; set; } = true;
 
         [Description("Bool indicating if ids of inner obejcts should be set or not. Only applicable of TypeAsRef is false.")]
