dfadaffc6e709b47c20aac7035cf6f0daad1ee9d
Isak Näslund <isak.naslund@burohappold.com>
2025-08-22 08:47:23 +0100

    Set descriptions for ref types, so they show up on properties, and add method docs for create methods

--- JsonSchema_Engine/Create/JsonSchema.cs
+++ JsonSchema_Engine/Create/JsonSchema.cs
@@ -13,6 +13,7 @@ namespace BH.Engine.JsonSchema
         /**** Public Methods                    ****/
         /*******************************************/
 
+        [Description("Creates a JSON Schema with a single keyword. This is typically used for simple schemas that only require one keyword, such as a type or item keyword.")]
         public static oM.JsonSchema.JsonSchema JsonSchemaSingleKeyword(ISchemaKeyWord keyWord)
         {
             return new oM.JsonSchema.JsonSchema { Keywords = new List<ISchemaKeyWord> { keyWord } };
@@ -20,13 +21,15 @@ namespace BH.Engine.JsonSchema
 
         /*******************************************/
 
-        public static oM.JsonSchema.JsonSchema JsonSchema(SchemaType schemaType, bool addNullIfNullable = false)
+        [Description("Creates a JsonSchema with the type keyword set to the provided schema type")]
+        public static oM.JsonSchema.JsonSchema JsonSchema(SchemaType schemaType, bool addNull = false)
         {
-            return JsonSchemaSingleKeyword(Create.TypeKeyword(schemaType));
+            return JsonSchemaSingleKeyword(Create.TypeKeyword(schemaType, addNull));
         }
 
         /*******************************************/
 
+        [Description("Creates a JSON Schema with a reference to another schema by its type. If the type does not have a schema ID, returns null.")]
         public static oM.JsonSchema.JsonSchema JsonSchema(Type type)
         {
             return JsonSchemaSingleKeyword(Create.TypeKeyword(type));
@@ -37,12 +40,15 @@ namespace BH.Engine.JsonSchema
         [Description("Creates a JSON Schema with a reference to another schema by its type and branch name. If the type does not have a schema ID, returns null.")]
         [Input("type", "The type for which the reference schema is being created. The type must implement IObject or be an enum in the BH.oM namespace.")]
         [Input("branchName", "The branch name to be used in the schema reference. This is typically the branch of the BHoM repository where the schema is located, e.g., 'develop' or 'v8.2'.")]
+        [Input("description", "The description text to be added to the schema. If null or empty, only the type's DescriptionAttribute will be used if available.")]
+        [Output("schema", "The JSON Schema with a reference to the specified type. If the type does not have a schema ID, returns null.")]
         public static oM.JsonSchema.JsonSchema RefJsonSchema(Type type, string branchName, string description)
         {
             var id = type.SchemaId(branchName);
             if (id != null)
             {
                 var schema = new oM.JsonSchema.JsonSchema { Keywords = new List<ISchemaKeyWord>() };
+                schema.AddDescription(type, description);   //Add descriptions for refs so that they do show up on properties.
                 schema.Keywords.Add(new RefKeyword() { Uri = id });
                 return schema;
             }
