72e41e9b173b82d2bbda25079d24b8db2f01ba6c
Isak Näslund <isak.naslund@burohappold.com>
2025-08-22 08:43:51 +0100

    Add methods for handling Id, Descriptions and references

--- JsonSchema_Engine/Create/JsonSchema.cs
+++ JsonSchema_Engine/Create/JsonSchema.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using BH.oM.Base.Attributes;
 using BH.oM.JsonSchema;
 
 namespace BH.Engine.JsonSchema
@@ -28,6 +30,23 @@ namespace BH.Engine.JsonSchema
         public static oM.JsonSchema.JsonSchema JsonSchema(Type type)
         {
             return JsonSchemaSingleKeyword(Create.TypeKeyword(type));
+        }
+
+        /*******************************************/
+
+        [Description("Creates a JSON Schema with a reference to another schema by its type and branch name. If the type does not have a schema ID, returns null.")]
+        [Input("type", "The type for which the reference schema is being created. The type must implement IObject or be an enum in the BH.oM namespace.")]
+        [Input("branchName", "The branch name to be used in the schema reference. This is typically the branch of the BHoM repository where the schema is located, e.g., 'develop' or 'v8.2'.")]
+        public static oM.JsonSchema.JsonSchema RefJsonSchema(Type type, string branchName, string description)
+        {
+            var id = type.SchemaId(branchName);
+            if (id != null)
+            {
+                var schema = new oM.JsonSchema.JsonSchema { Keywords = new List<ISchemaKeyWord>() };
+                schema.Keywords.Add(new RefKeyword() { Uri = id });
+                return schema;
+            }
+            return null;
         }
 
         /*******************************************/
---
+++ JsonSchema_Engine/Modify/AddDescription.cs
@@ -0,0 +1,38 @@
+﻿using BH.oM.Base.Attributes;
+using BH.oM.JsonSchema;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BH.Engine.JsonSchema
+{
+    public static partial class Modify 
+    {
+        /*******************************************/
+        /**** Public Methods                    ****/
+        /*******************************************/
+
+        [Description("Adds a DescriptionKeyword to the schema, using the provided description and the type's DescriptionAttribute if available.")]
+        [Input("schema", "The JSON Schema to which the description will be added.")]
+        [Input("type", "The type for which the description is being added. If the type has a DescriptionAttribute, its value will be included in the schema description.")]
+        [Input("description", "The description text to be added to the schema. If null or empty, only the type's DescriptionAttribute will be used if available.")]
+        public static void AddDescription(this oM.JsonSchema.JsonSchema schema, Type type, string description)
+        {
+            string typeDesc = type.GetCustomAttribute<DescriptionAttribute>()?.Description;
+            description = description ?? "";
+            if (typeDesc != null)
+            {
+                description += $" {type.Name}: {typeDesc}";
+            }
+            description = description.Trim();
+
+            if (!string.IsNullOrWhiteSpace(description))   //If description set, add to schema
+                schema.Keywords.Add(new DescriptionKeyword { Description = description });
+        }
+
+        /*******************************************/
+    }
+}
